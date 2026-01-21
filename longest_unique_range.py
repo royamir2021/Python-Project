13900228988218806f17aad6fa6d18becaf2b80a
Roya Mirzaei <royamir2021@gmail.com>
2026-01-20 20:05:33 -0500

    added my method with deleting from Dictionary

--- longest_unique_range.py
+++ longest_unique_range.py
@@ -30,3 +30,35 @@ def longest_unique_range(nums):
             best_r = right
 
     return (best_len, (best_l, best_r))
+ # my method with deleting from Dictionary
+def longest_unique_range_d(nums):
+    """
+    Returns: (max_len, (start, end)) for the longest contiguous subarray with all unique values.
+    If nums is empty: (0, (-1, -1))
+    """
+    if not nums:
+        return (0, (-1, -1))
+
+    last_seen = {}          # value -> last index
+    left = 0
+
+    best_len = 0
+    best_l = 0
+    best_r = 0
+
+    for right, x in enumerate(nums):
+        # If x was seen inside the current window, jump left past its previous index
+        if x in last_seen and last_seen[x] >= left:
+            left = last_seen[x] + 1
+
+        # Update last seen position of x
+        last_seen[x] = right
+
+        # Update best window
+        cur_len = right - left + 1
+        if cur_len > best_len:
+            best_len = cur_len
+            best_l = left
+            best_r = right
+
+    return (best_len, (best_l, best_r))
