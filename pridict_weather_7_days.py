89ed777177e2d5d88428e82a98f6d7d4310c15c6
Roya Mirzaei <royamir2021@gmail.com>
2026-01-21 18:06:51 -0500

    added Param septayly to reach and expand more easy

--- pridict_weather_7_days.py
+++ pridict_weather_7_days.py
@@ -10,8 +10,16 @@ week_ago = today - timedelta(days=7)
 start_date = week_ago.strftime("%Y-%m-%d")
 end_date = today.strftime("%Y-%m-%d")
 
-url = f"https://api.open-meteo.com/v1/forecast?latitude=48.85&longitude=2.35&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min"
-response = requests.get(url)
+params = {
+    "latitude": 43.65107,
+    "longitude": -79.347015,
+    "start_date": start_date,
+    "end_date": end_date,
+    "daily": "temperature_2m_max,temperature_2m_min",
+    "timezone": "America/Toronto"
+}
+
+response = requests.get("https://api.open-meteo.com/v1/forecast", params=params)
 data = response.json()
 
 # 2. Process with pandas
@@ -32,7 +40,7 @@ plt.plot(df['date'], df['avg_temp'], 'g--', label='Average')
 
 plt.xlabel('Date')
 plt.ylabel('Temperature (°C)')
-plt.title('Paris Weather - Past Week')
+plt.title('Toronto_weatherr - Past Week')
 plt.legend()
 plt.grid(True, alpha=0.3)
 plt.xticks(rotation=45)
@@ -43,7 +51,7 @@ if not os.path.exists('data'):
     os.makedirs('data')
 
 plt.savefig('data/weather_chart.png')
-df.to_csv('data/paris_weather.csv', index=False)
+df.to_csv('data/،Toronto_weather.csv', index=False)
 
 print(f"Average temperature: {df['avg_temp'].mean():.1f}°C")
 print("Files saved in 'data' folder")
